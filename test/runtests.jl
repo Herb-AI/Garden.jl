bd9d1d2a249c2c5826fcfaf1224ba9b4cf4d5d99
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2024-07-05 13:47:02 +0200

    Skip JET for julia <1.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,10 @@ using JET
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(Garden)
     end
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(Garden; target_defined_modules = true)
+    if VERSION >= v"1.7"
+        @testset "Code linting (JET.jl)" begin
+            JET.test_package(Garden; target_defined_modules = true)
+        end
     end
     # Write your tests here.
 end
